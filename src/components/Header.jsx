e8b182d3a933b088a287c7bcf01a1160477c9d1c
Zain Saleem <zain.s97@outlook.com>
2020-07-15 23:04:25 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Routes, Link, Route } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Products from './Products'
+import NotFound from './NotFound'
+import ProductDetails from './ProductDetails'
+
+const Header = () => {
+    return (
+        <div>
+            <Link to="/">Home</Link>{" | "}
+            <Link to="products">Products</Link>{" | "}
+            <Link to="about">About</Link>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="products" element={<Products />}>
+                    <Route path=":productID" element={<ProductDetails />} />
+                </Route>
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            Please enter the correct URL.
+        </div>
+    )
+}
+
+export default NotFound
